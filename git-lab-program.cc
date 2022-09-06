fc0ffab7a462ac85660c9a0f24a4f7c9ba3e3d52
Chris <cd012120@ohio.edu>
2022-09-06 18:30:52 -0400

    Struggling

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Chris DeLeon-Mettee
+ *        Date: 9/6/2022
+ * Description: 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
